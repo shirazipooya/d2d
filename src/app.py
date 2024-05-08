32ecbb93846a94e27b5d6b6b51a9f0d4532905ab
Pooya Shirazi <p.shirazi.a@gmail.com>
2024-05-08 08:58:33 +0330

    ...

--- src/app.py
+++ src/app.py
@@ -1,33 +1,16 @@
-import os
-import redis
-from flask import Flask
 import dash
-from dash import Dash, html, dcc
+from flask import Flask
+from dash import Input, Output, State, html
 import dash_bootstrap_components as dbc
-import dash_mantine_components as dmc
-from dash_dangerously_set_inner_html import DangerouslySetInnerHTML
-from dotenv import load_dotenv
-
-from src.components import sidebar
-
-
-# Load Environment Variables
-load_dotenv(".env")
-
-# REDIS DB
-r = redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"))
 
 
 external_stylesheets = [
+    dbc.themes.BOOTSTRAP,
     "/static/vendor/fontawesome/v6.5.1/css/fontawesome.min.css",
     "/static/vendor/fontawesome/v6.5.1/css/solid.min.css",
-    "/static/vendor/fontawesome/v6.5.1/css/brands.min.css",    
-    "/static/vendor/bootstrap/v5.3.3/css/bootstrap.min.css",    
+    "/static/vendor/fontawesome/v6.5.1/css/brands.min.css",
+    "/static/vendor/bootstrap/v5.3.3/css/bootstrap.min.css",
     "/static/css/style.css",
-    # dbc.icons.BOOTSTRAP,
-    # dbc.themes.FLATLY,
-    # dbc.themes.BOOTSTRAP,
-    # dbc.icons.FONT_AWESOME,
 ]
 
 external_scripts = [
@@ -40,8 +23,9 @@ external_scripts = [
 
 server = Flask(__name__)
 
-app = Dash(
-    __name__,
+
+app = dash.Dash(
+    name=__name__,
     server=server,
     use_pages=True,
     pages_folder="pages",
@@ -61,17 +45,74 @@ app = Dash(
 )
 
 
-def app_layout():
-    return html.Div(
-        children=[
-            sidebar.sidebar(),
-            dash.page_container,
-        ]
+sidebar_header = dbc.Row(
+    [
+        dbc.Col(
+            html.H5(
+                "Pooya's Dashboard",
+            ),
+            style={"text-align": "center"}
+        ),
+        dbc.Col(
+            [
+                html.Button(
+                    html.Span(className="navbar-toggler-icon"),
+                    className="navbar-toggler",
+                    style={
+                        "color": "rgba(0,0,0,.5)",
+                        "border-color": "rgba(0,0,0,.1)",
+                    },
+                    id="navbar-toggle",
+                ),
+            ],
+            width="auto",
+            align="center",
+        ),
+    ],
+)
+
+sidebar = html.Div(
+    [
+        sidebar_header,
+        dbc.Collapse(
+            dbc.Nav(
+                [
+                    dbc.NavLink(page["name"], href=page["path"], active="exact")
+                    for page in dash.page_registry.values()
+                    if page["module"] != "pages.404"
+                ],
+                vertical=True,
+                pills=True,
+                className="my-0 py-0"
+            ),
+            id="collapse",
+        ),
+    ],
+    id="sidebar",
+)
+
+
+app.layout = html.Div(
+    children=[
+        sidebar,
+        dash.page_container
+    ],
+)
+
+
+@app.callback(
+    Output("collapse", "is_open"),
+    [Input("navbar-toggle", "n_clicks")],
+    [State("collapse", "is_open")],
+)
+def toggle_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+
+if __name__ == "__main__":
+    app.run_server(
+        debug=True
     )
-
-
-app.layout = app_layout
-
-
-if __name__ == '__main__':
-    app.run_server()
+    
--- src/pages/home.py
+++ src/pages/home.py
@@ -1,17 +1,85 @@
+import plotly.express as px
+from dash import Dash, dcc, html, Input, Output, callback
+import dash_bootstrap_components as dbc
 import dash
-from dash import html
-import dash_mantine_components as dmc
-
-
 
 dash.register_page(
-    __name__,
-    title='Home',
-    name='Home',
-    path='/',
+    module=__name__,
+    name="Home",
+    path="/"
 )
+
+
+def jumbotron():
+    return html.Div(
+        dbc.Container(
+            [
+                html.H1("Jumbotron", className="display-3"),
+                html.P(
+                    "Use Containers to create a jumbotron to call attention to "
+                    "featured content or information.",
+                    className="lead",
+                ),
+                html.Hr(className="my-2"),
+                html.P(
+                    "Use utility classes for typography and spacing to suit the "
+                    "larger container."
+                ),
+                html.P(
+                    dbc.Button("Learn more", color="primary"), className="lead"
+                ),
+            ],
+            fluid=True,
+            className="py-3",
+        ),
+        className="p-1 m-1 bg-body-secondary rounded-3",
+    )
+
+
+def card():
+    return html.Div([
+        dbc.Card(
+            [
+                dbc.CardHeader("This is the header"),
+                dbc.CardBody(
+                    [
+                        html.H4("Card title", className="card-title"),
+                        html.P("This is some card text", className="card-text"),
+                    ]
+                ),
+                dbc.CardFooter("This is the footer"),
+            ],
+        )
+    ])
 
 
 layout = html.Div(
-
+    id="page-content",
+    children=[
+        html.Div(id='', className='row', children=[
+            html.Div(id='', className='col-sm-12 col-md-12 col-lg-6', children=[
+                jumbotron()
+            ]),
+            html.Div(id='', className='col-sm-12 col-md-12 col-lg-6', children=[
+                jumbotron()
+            ]),
+        ]),
+        html.Div(id='', className='row mt-2', children=[
+            html.Div(id='', className='col-sm-12 col-md-12 col-lg-4 my-1', children=[
+                card()
+            ]),
+            html.Div(id='', className='col-sm-12 col-md-12 col-lg-4 my-1', children=[
+                card()
+            ]),
+            html.Div(id='', className='col-sm-12 col-md-12 col-lg-4 my-1', children=[
+                card()
+            ]),
+        ])
+    ]
 )
+
+
+# @callback(Output("heatmaps-graph", "figure"), Input("heatmaps-medals", "value"))
+# def filter_heatmap(cols):
+#     fig = px.imshow(df[cols])
+#     return fig
